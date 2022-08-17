68a51f27cb86c90eea8007c95e0bd3efe7aff004
yamrire <yamrire@student.1337.ma>
2022-08-17 21:26:12 +0100

    map structure

--- main.c
+++ main.c
@@ -3,127 +3,196 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <yamrire@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 02:21:58 by yamrire           #+#    #+#             */
-/*   Updated: 2022/08/16 03:31:03 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/08/17 21:21:40 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <stdio.h>
 
-void	check_map_name(char *av)
+// check line format
+int check_line(char *line, int len, int *player)
 {
-	char	*ext;
-	int		status;
 
-	ext = ft_substr(av, ft_strlen(av) - 4, 4);
-	status = ft_strncmp(ext, ".ber", ft_strlen(ext));
-	if (status != 0)
+	int index = 0;
+
+	// start with 1 and end with 1
+	if (line[0] != '1' || line[len - 2] != '1')
+		return -1;
+	// check valid characters
+	while (line[index])
 	{
-		ft_printf("ERROR : Map name incorrect !\n");
-		exit (-1);
+		if (line[index] != '0' && line[index] != '1' && line[index] != 'C' && line[index] != 'E' && line[index] != 'P')
+			return -1;
+		// player
+		if (line[index] == 'P')
+		{
+			if (*player != 0)
+				*player += 1;
+			else
+				return -1;
+		}
 	}
-}
-
-char	**map_valid_dimension(char *av)
-{
-	char	**map;
-	int		fd;
-	int		len;
-	int		len_check;
-	int		index;
-	char	*map_tmp;
-	char	*tmp1;
-	char	*tmp2;
-	int		p_status;
-
-	fd = open(av, O_RDONLY);
-	tmp1 = get_next_line(fd);
-	len = ft_strlen(tmp1);
-	index = 0;
-	while (tmp1[index])
-	{
-		if (tmp1[index] != '1')
-		{
-			ft_printf("ERROR : First wall structure incorrect !\n");
-			exit (-1);
-		}
-		index++;
-	}
-	map_tmp = ft_strdup(tmp1);
-	free (tmp1);
-	index = 1;
-	len_check = 0;
-	p_status = 0;
-	while (tmp1 = get_next_line(fd))
-	{
-		len_check = ft_strlen(tmp1);
-		if (len_check != len)
-		{
-			ft_printf("ERROR : Line lenght incorrect !\n");
-			exit (-1);
-		}
-		if (tmp1[0] != '1' || tmp1[len_check - 2] != '1')
-		{
-			ft_printf("ERROR : Wall structure incorrect !\n");
-			exit (-1);
-		}
-		while (tmp1[index] && index < len_check - 2)
-		{
-			if (tmp1[index] != '0' || tmp1[index] != '1' || tmp1[index] != 'C' || tmp1[index] != 'E' || tmp1[index] != 'P')
-			{
-				ft_printf("ERROR : Map structure incorrect !\n");
-				exit (-1);
-			}
-			if (tmp1[index] == 'P')
-			{
-				if (p_status != 0)
-					p_status += 1;
-				else
-				{
-					ft_printf("ERROR : You only need 1 player !\n");
-					exit (-1);
-				}
-			}
-			index++;
-		}
-		tmp2 = ft_strdup(map_tmp);
-		free(map_tmp);
-		map_tmp = ft_strjoin(tmp2, tmp1);
-		free(tmp1);
-		free(tmp2);
-	}
-}
-
-int key_hook(int key, t_data *mlx)
-{
-	printf("key : %d\n", key);
-	mlx->x += 100;
-	mlx_destroy_image(mlx->ptr, mlx->img);
-	mlx_clear_window(mlx->ptr, mlx->win);
-	mlx->img = mlx_xpm_file_to_image(mlx->ptr, "./ralph.xpm", &mlx->img_width, &mlx->img_height);
-	mlx_put_image_to_window(mlx->ptr, mlx->win, mlx->img, mlx->x, mlx->y);
 	return 0;
 }
 
-int	main(void)
+// check walls (first line and last line)
+int check_wall(char *line)
 {
-	t_data	mlx;
-	char	*relative_path;
 
-	mlx.x = 100;
-	mlx.y = 100;
-	mlx.win_width = 600;
-	mlx.win_height = 600;
+	int index = 0;
 
-	mlx.ptr = mlx_init();
-	relative_path = "./ralph.xpm";
-	mlx.win = mlx_new_window(mlx.ptr, mlx.win_width, mlx.win_height, "so_long");
-	mlx.img = mlx_xpm_file_to_image(mlx.ptr, relative_path, &mlx.img_width, &mlx.img_height);
-	mlx_put_image_to_window(mlx.ptr, mlx.win, mlx.img, 100, 100);
-	mlx_key_hook(mlx.win, key_hook, &mlx);
-	mlx_loop(mlx.ptr);
+	while (line[index])
+	{
+		if (line[index] != '1')
+			return -1;
+		index++;
+	}
+	return 0;
+}
 
+/*
+
+-> check file and file extension
+
+-> while (line) {
+	-> check line
+	if line is not valid
+		-> error
+	else
+		-> save line
+	-> go to next line
+}
+
+split all saved lines to 2D array
+
+map = split(lines);
+
+check first line and last line
+
+check_wall(map[0]) && check_wall(map[n])
+if not valid error
+
+return map;
+
+*/
+
+int check_map_file(char *av)
+{
+	char *ext;
+	int status;
+	int fd;
+
+	ext = ft_substr(av, ft_strlen(av) - 4, 4);
+	status = ft_strncmp(ext, ".ber", ft_strlen(ext));
+	// check file
+	fd = open(av, O_RDONLY);
+	if (status != 0 || fd == -1)
+	{
+		printf("ERROR : Map name incorrect !\n");
+		exit(-1);
+	}
+	return fd;
+}
+
+void ft_exit(char *message)
+{
+	printf("%s\n", message);
+	exit(-1);
+}
+
+int count_line(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+	if (s[i - 1] == '\n')
+		i--;
+	return i;
+}
+
+void save_line(char *dist, char *line)
+{
+	char *tmp;
+	tmp = ft_strjoin(dist, line);
+	free(dist);
+	dist = tmp;
+}
+
+char **map_valid_dimension(char *av)
+{
+	char **map;
+	char *saved_lines;
+	char *line;
+	int fd;
+	int len;
+	int p_status;
+	int ret;
+
+	p_status = 0;
+	len = 0;
+	saved_lines = ft_strdup("");
+	fd = check_map_file(av);
+
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break;
+		if (len == 0)
+			len = count_line(line);
+		else if (len != count_line(line))
+			ft_exit("lines...");
+		// check line
+		ret = check_line(line, len, &p_status);
+		if (ret == -1)
+			ft_exit("invalid map structure");
+		// save line
+		save_line(saved_lines, line);
+		free(line);
+	}
+	map = ft_split(saved_lines, '\n');
+	return (map);
+}
+
+// int key_hook(int key, t_data *mlx)
+// {
+// 	printf("key : %d\n", key);
+// 	mlx->x += 100;
+// 	mlx_destroy_image(mlx->ptr, mlx->img);
+// 	mlx_clear_window(mlx->ptr, mlx->win);
+// 	mlx->img = mlx_xpm_file_to_image(mlx->ptr, "./ralph.xpm", &mlx->img_width, &mlx->img_height);
+// 	mlx_put_image_to_window(mlx->ptr, mlx->win, mlx->img, mlx->x, mlx->y);
+// 	return 0;
+// }
+
+int main(int ac, char **av)
+{
+	// t_data	mlx;
+	// char	*relative_path;
+
+	// mlx.x = 100;
+	// mlx.y = 100;
+	// mlx.win_width = 600;
+	// mlx.win_height = 600;
+
+	// mlx.ptr = mlx_init();
+	// relative_path = "./ralph.xpm";
+	// mlx.win = mlx_new_window(mlx.ptr, mlx.win_width, mlx.win_height, "so_long");
+	// mlx.img = mlx_xpm_file_to_image(mlx.ptr, relative_path, &mlx.img_width, &mlx.img_height);
+	// mlx_put_image_to_window(mlx.ptr, mlx.win, mlx.img, 100, 100);
+	// mlx_key_hook(mlx.win, key_hook, &mlx);
+	// mlx_loop(mlx.ptr);
+
+	char **map = map_valid_dimension(av[1]);
+	int index = 0;
+	while (map[index])
+	{
+		printf("%s\n", map[index]);
+		index++;
+	}
+	return 0;
 }
