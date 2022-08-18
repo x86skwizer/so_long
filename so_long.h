2d8aa617465f8ed33da49a8ea8f2233406b69c21
Yassine Amrire <yamrire@e2r10p4.1337.ma>
2022-08-18 05:16:55 +0100

    add s_counter struct

--- so_long.h
+++ so_long.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: yamrire <yamrire@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 00:44:55 by yamrire           #+#    #+#             */
-/*   Updated: 2022/08/17 01:17:02 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/08/18 03:34:33 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-// # include <mlx.h>
+# include <mlx.h>
 # include <fcntl.h>
 # include "get_next_line.h"
 # include "libftprintf.h"
@@ -30,6 +30,25 @@ typedef struct s_data
 	int		x;
 	int		y;
 }	t_data;
+
+typedef struct s_space
+{
+	char **map;
+	char *saved_lines;
+	char *line;
+	int fd;
+	int len;
+	int p_status;
+	int ret;
+	int counter;
+}	t_space;
+
+typedef struct s_counter
+{
+	int	i;
+	int	j;
+}	t_counter;
+
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char	**ft_split(char const *s, char c);
